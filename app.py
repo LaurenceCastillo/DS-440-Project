5f1ea6bf71d09bd865fe03fdf9e5598c5e643e4a
Laurence Castillo <laurence.castillo@me.com>
2024-11-07 18:37:39 -0500

    Heroku setup

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 
-#repository will be deployed to herokus o that algorithm can be run remotely through wix.
+#repository will be deployed to herokus so that algorithm can be triggered by wix.
 
 app = Flask(__name__)
 
 @app.route('/run-script', methods=['POST'])
 def run_script():
+
+   #doesn't do anything yet
 
     result = {"message": "Script ran successfully!"}
     return jsonify(result)
